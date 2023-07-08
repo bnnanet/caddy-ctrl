0ea7b33bddca817e9ab1f092e6eea1c63b85cb3e
AJ ONeal <aj@therootcompany.com>
2023-07-08 14:21:35 -0600

    f: add init admin

--- init.js
+++ init.js
@@ -2,6 +2,133 @@
 
 let Caddy = module.exports;
 
+let myDebugLevel = "INFO";
+let myAdminDomain = "sni.m.example.com";
+let myAdminIp = "192.168.1.10";
+
+let myAdminSshForward = {
+  match: [
+    {
+      tls: {
+        sni: [myAdminDomain],
+      },
+    },
+  ],
+  handle: [
+    {
+      handler: "tls",
+      connection_policies: [
+        {
+          alpn: ["http/1.1"],
+        },
+      ],
+    },
+    {
+      handler: "subroute",
+      routes: [
+        {
+          match: [
+            {
+              ssh: {},
+            },
+          ],
+          handle: [
+            {
+              handler: "proxy",
+              upstreams: [
+                {
+                  dial: ["localhost:22"],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          match: [
+            {
+              http: [
+                {
+                  host: [myAdminDomain],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+let myAdminApi = {
+  match: [
+    {
+      host: [myAdminDomain],
+    },
+  ],
+  handle: [
+    {
+      handler: "subroute",
+      routes: [
+        {
+          handle: [
+            {
+              handler: "authentication",
+              providers: {
+                http_basic: {
+                  accounts: [
+                    {
+                      password: "{env.CADDY_BCRYPT}",
+                      username: "{env.CADDY_USER}",
+                    },
+                  ],
+                  hash: {
+                    algorithm: "bcrypt",
+                  },
+                  hash_cache: {},
+                },
+              },
+            },
+            {
+              handler: "reverse_proxy",
+              headers: {
+                request: {
+                  set: {
+                    Host: ["localhost:2019"],
+                  },
+                },
+              },
+              upstreams: [
+                {
+                  dial: "localhost:2019",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  terminal: true,
+};
+
+let redirectToHttps = {
+  listen: [":80"],
+  routes: [
+    {
+      handle: [
+        {
+          handler: "static_response",
+          headers: {
+            Location: ["https://{http.request.host}{http.request.uri}"],
+          },
+          status_code: 302,
+        },
+      ],
+      terminal: true,
+    },
+  ],
+};
+
 Caddy.init = {
   admin: {
     config: {
@@ -11,7 +138,7 @@ Caddy.init = {
   logging: {
     logs: {
       default: {
-        level: "'${my_debug_level}'",
+        level: myDebugLevel,
       },
     },
   },
@@ -23,59 +150,7 @@ Caddy.init = {
           automatic_https: {
             disable: true,
           },
-          routes: [
-            {
-              match: [
-                {
-                  host: ["'${my_admin_domain}'"],
-                },
-              ],
-              handle: [
-                {
-                  handler: "subroute",
-                  routes: [
-                    {
-                      handle: [
-                        {
-                          handler: "authentication",
-                          providers: {
-                            http_basic: {
-                              accounts: [
-                                {
-                                  password: "{env.CADDY_BCRYPT}",
-                                  username: "{env.CADDY_USER}",
-                                },
-                              ],
-                              hash: {
-                                algorithm: "bcrypt",
-                              },
-                              hash_cache: {},
-                            },
-                          },
-                        },
-                        {
-                          handler: "reverse_proxy",
-                          headers: {
-                            request: {
-                              set: {
-                                Host: ["localhost:2019"],
-                              },
-                            },
-                          },
-                          upstreams: [
-                            {
-                              dial: "localhost:2019",
-                            },
-                          ],
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-              terminal: true,
-            },
-          ],
+          routes: [myAdminApi],
           listener_wrappers: [
             {
               wrapper: "layer4",
@@ -89,60 +164,7 @@ Caddy.init = {
                   handle: [
                     {
                       handler: "subroute",
-                      routes: [
-                        {
-                          match: [
-                            {
-                              tls: {
-                                sni: ["'${my_admin_domain}'"],
-                              },
-                            },
-                          ],
-                          handle: [
-                            {
-                              handler: "tls",
-                              connection_policies: [
-                                {
-                                  alpn: ["http/1.1"],
-                                },
-                              ],
-                            },
-                            {
-                              handler: "subroute",
-                              routes: [
-                                {
-                                  match: [
-                                    {
-                                      ssh: {},
-                                    },
-                                  ],
-                                  handle: [
-                                    {
-                                      handler: "proxy",
-                                      upstreams: [
-                                        {
-                                          dial: ["localhost:22"],
-                                        },
-                                      ],
-                                    },
-                                  ],
-                                },
-                                {
-                                  match: [
-                                    {
-                                      http: [
-                                        {
-                                          host: ["'${my_admin_domain}'"],
-                                        },
-                                      ],
-                                    },
-                                  ],
-                                },
-                              ],
-                            },
-                          ],
-                        },
-                      ],
+                      routes: [myAdminSshForward],
                     },
                   ],
                 },
@@ -150,34 +172,19 @@ Caddy.init = {
             },
           ],
         },
-        srv80: {
-          listen: [":80"],
-          routes: [
-            {
-              handle: [
-                {
-                  handler: "static_response",
-                  headers: {
-                    Location: ["https://{http.request.host}{http.request.uri}"],
-                  },
-                  status_code: 302,
-                },
-              ],
-              terminal: true,
-            },
-          ],
-        },
+        // redirect to https
+        srv80: redirectToHttps,
       },
     },
     tls: {
       certificates: {
-        automate: ["localhost", "'${my_admin_ip}'", "'${my_admin_domain}'"],
+        automate: ["localhost", myAdminIp, myAdminDomain],
       },
       automation: {
         policies: [
           {
             "@id": "caddy_admin_tls_policy_internal",
-            subjects: ["localhost", "'${my_admin_ip}'"],
+            subjects: ["localhost", myAdminIp],
             issuers: [
               {
                 module: "internal",
@@ -186,7 +193,7 @@ Caddy.init = {
           },
           {
             "@id": "caddy_admin_tls_policy_acme",
-            subjects: ["'${my_admin_domain}'"],
+            subjects: [myAdminDomain],
             issuers: [
               {
                 module: "acme",
